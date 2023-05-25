3cdd890cd9a8178eb814b02f70140f59b33d82df
administrator1 <administrator1@DESKTOP-S60QF22>
2023-05-25 12:19:42 +0900

    first commit

---
+++ CampleProj/src/main/webapp/assets/js/login.js
@@ -0,0 +1,21 @@
+const signInBtn = document.getElementById("signIn");
+const signUpBtn = document.getElementById("signUp");
+const fistForm = document.getElementById("form1");
+const secondForm = document.getElementById("form2");
+const container = document.querySelector(".container");
+
+console.log(signInBtn);
+console.log(signUpBtn);
+console.log(fistForm);
+console.log(secondForm);
+console.log(container);
+signInBtn.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
+});
+
+signUpBtn.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
+});
+
+fistForm.addEventListener("submit", (e) => e.preventDefault());
+secondForm.addEventListener("submit", (e) => e.preventDefault());
